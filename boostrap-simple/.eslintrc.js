7d6444eab530a3c25d2c731e79a33614f8f34f70
Zsolt Lengyel <zsolt.lengyel21@gmail.com>
2023-01-13 12:33:13 +0100

    bootstrap

---
+++ boostrap-simple/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es6': true
+  },
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'no-console': 0,
+    'no-redeclare': 0,
+    'no-unused-vars': 0,
+    'no-undef': 0,
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ]
+  }
+};
